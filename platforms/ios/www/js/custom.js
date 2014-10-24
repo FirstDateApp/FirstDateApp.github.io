0e52e96ce448552b6d7dc2f3339a979d01711ae9
rich sollenne <rich@sollenne.com>
2014-10-24 17:33:58 -0500

    remove some stuff that didn't work
    Signed-off-by: rich sollenne <rich@sollenne.com>

--- platforms/ios/www/js/custom.js
+++ platforms/ios/www/js/custom.js
@@ -1,6 +1,3 @@
 $(document).ready(function() {
-	$(function() {
-	  $(document).on("touchmove", function(evt) { evt.preventDefault() });
-	  $(document).on("touchmove", ".scrollable", function(evt) { evt.stopPropagation() });
-	});
+
 });
